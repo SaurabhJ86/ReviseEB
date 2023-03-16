fd424e9ec1b9cd6bb0e5f32f1aa0d02511d6039b
Saurabh Jhingan <saurabh.jhingan.08@gmail.com>
2023-03-16 16:02:39 +0530

    Updated the return message

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello_world():
-    return "Hello World, this is your Lord, Sir Henser Dufrey."
+    return "Hello World, this is your Lord, Sir Henser Dufrey. This shall be taken along with a sense of urgency."
